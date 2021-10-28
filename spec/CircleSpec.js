932d6c9e45ac16489c71ccdc97850767a092574c
Anmol <bhatianmol566@yahoo.co.uk>
2021-10-28 15:45:30 +0100

    Add tests for circle position

--- spec/CircleSpec.js
+++ spec/CircleSpec.js
@@ -9,4 +9,15 @@ describe("Circle", function() {
         expect(circle.radius).toEqual(100);
     });
 
+    it("should have a default x and a y position", function() {
+        expect(circle.getCoords()).toEqual([0, 0]);
+    });
+
+    describe('#randomisePostition', function() {
+        it("returns a random postition between a given 2d range", function() {
+            spyOn(Math, 'random').and.returnValue(1);
+            circle.randomisePosition(10, 10);
+            expect(circle.getCoords()).toEqual([10, 10]);
+        });
+    });
 });
--- src/Circle.js
+++ src/Circle.js
@@ -1,5 +1,20 @@
 class Circle {
     constructor() {
         this.radius = 100;
+        this.x = 0;
+        this.y = 0;
     }
+
+    getCoords() {
+        return [this.x, this.y];
+    }
+
+    randomisePosition(x, y) {
+        this.x = getRandomInt(x);
+        this.y = getRandomInt(y);
+    }
+}
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
 }
